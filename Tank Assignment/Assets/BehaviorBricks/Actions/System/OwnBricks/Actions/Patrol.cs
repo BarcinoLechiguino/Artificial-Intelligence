02268cb1de0171ac8a70f69db0e5301eb5fd3973
Ángel González <angotov@gmail.com>
2020-12-08 04:15:11 +0100

    Added the Behaviour Bricks Extension and Added the required scripts to start coding the bricks.

---
+++ Tank Assignment/Assets/BehaviorBricks/Actions/System/OwnBricks/Actions/Patrol.cs
@@ -0,0 +1,33 @@
+﻿using Pada1.BBCore.Tasks;
+using Pada1.BBCore;
+using UnityEngine;
+
+namespace BBUnity.Actions
+{
+    [Action("OwnBricks/Actions/Patrol")]
+    [Help("Moves the game object to a given position by using a NavMeshAgent")]
+    public class Patrol : GOAction
+    {
+        ///<value>Input target position where the game object will be moved Parameter.</value>
+        [InParam("target")]
+        [Help("Target position where the game object will be moved")]
+        public Vector3 target;
+
+        [OutParam("target_out")]
+        public Vector3 target_out;
+
+
+        public override void OnStart()
+        {
+
+        }
+
+        public override TaskStatus OnUpdate()
+        {
+            return TaskStatus.COMPLETED;
+        }
+    }
+}
+
+/*private UnityEngine.AI.NavMeshAgent navAgent;
+if (!navAgent.pathPending && navAgent.remainingDistance <= navAgent.stoppingDistance)*/
---
+++ Tank Assignment/Assets/BehaviorBricks/Actions/System/OwnBricks/Actions/ShotCooldown.cs
@@ -0,0 +1,30 @@
+﻿using Pada1.BBCore.Tasks;
+using Pada1.BBCore;
+using UnityEngine;
+
+namespace BBUnity.Actions
+{
+    [Action("OwnBricks/Actions/ShotCooldown")]
+    [Help("Moves the game object to a given position by using a NavMeshAgent")]
+    public class ShotCooldown : GOAction
+    {
+        ///<value>Input target position where the game object will be moved Parameter.</value>
+        [InParam("target")]
+        [Help("Target position where the game object will be moved")]
+        public Vector3 target;
+
+        [OutParam("target_out")]
+        public Vector3 target_out;
+
+
+        public override void OnStart()
+        {
+
+        }
+
+        public override TaskStatus OnUpdate()
+        {
+            return TaskStatus.COMPLETED;
+        }
+    }
+}
